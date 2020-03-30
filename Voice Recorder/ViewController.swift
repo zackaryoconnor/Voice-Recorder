d63595af7bcf45b1fef785407b17eb8af4d6fb78
Zackary O'Connor <zackaryoconnor@Zackarys-iMac.local>
2020-03-30 18:16:41 -0400

    Initial commit

---
+++ Voice Recorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Voice Recorder
+//
+//  Created by Zackary O'Connor on 3/30/20.
+//  Copyright © 2020 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
